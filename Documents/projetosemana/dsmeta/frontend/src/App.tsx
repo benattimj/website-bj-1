febd32f4a16f3462c8269b854c5e73f75e9510e8
Murilo Benatti <benatti1104@gmail.com>
2022-09-09 19:30:50 -0300

    Security config

---
+++ Documents/projetosemana/dsmeta/frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import Header from "./componentes/Header";
+import SalesCard from "./componentes/SalesCard";
+
+function App() {
+
+  return (
+<>
+
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+
+          </div>
+        </section>
+      </main>
+</>
+      )
+}
+
+
+
+      export default App;
